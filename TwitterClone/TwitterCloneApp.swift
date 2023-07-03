8390fcf04e20c8cd8c458fbfff4ad5e14110b830
Shalini Ramalingam <shalini.munch@gmail.com>
2023-07-03 22:09:23 +0200

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Ramdhas on 2023-07-03.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
